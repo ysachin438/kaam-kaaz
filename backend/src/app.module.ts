0076454974e41eb5f1555d50edc87d3819d98cd8
Sachin <sachin.yadav@appsquadz.co>
2025-06-17 11:11:17 +0530

    Database Modularization

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,20 +3,11 @@ import { UserModule } from './Users/users.module';
 import { AuthModule } from './Users/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { users } from "./Users/entities/user.entity"
-import { tasks } from "./Users/entities/task.entity";
+import { tasks } from "./Users/entities/task.entity"
+import { mysqlConfig } from "./config/database.config"
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '@Nubhav438',
-      database: 'node_nest',
-      // entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      entities: [users, tasks],
-      synchronize: true,
-    }),
+    TypeOrmModule.forRoot(mysqlConfig),
     TypeOrmModule.forFeature([users, tasks]),
     UserModule,
     AuthModule
---
+++ backend/src/config/database.config.ts
@@ -0,0 +1,15 @@
+import { users } from "src/Users/entities/user.entity"
+import { tasks } from "src/Users/entities/task.entity"
+import { TypeOrmModuleOptions } from "@nestjs/typeorm"
+
+export const mysqlConfig: TypeOrmModuleOptions = {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: '@Nubhav438',
+    database: 'node_nest',
+    // entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    entities: [users, tasks],
+    synchronize: true,
+}
