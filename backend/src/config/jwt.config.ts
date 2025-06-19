5c69d79ebebf95ee2270f626fa316ea4c5ff94c1
Sachin <sachin.yadav@appsquadz.co>
2025-06-19 10:48:35 +0530

    Minor Fixes

--- backend/src/config/jwt.config.ts
+++ backend/src/config/jwt.config.ts
@@ -1,6 +1,8 @@
-export const jwtConfig = {
-    secret: 'qwertyuiop1234567890',
+import { ConfigService } from "@nestjs/config";
+
+export const jwtConfig = (configService: ConfigService) => ({
+    secret: configService.get<string>('JWT_SECRET'),
     signOptions: {
-        expiresIn: '1d',
+        expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
     },
-}; 
+}); 
